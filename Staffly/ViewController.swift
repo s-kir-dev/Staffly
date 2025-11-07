51461bea0a35f30adac870476bcc9529cc1a701f
s_kir <syskirill1@gmail.com>
2025-11-07 16:42:50 +0300

    Initial Commit

---
+++ Staffly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Staffly
+//
+//  Created by Kirill Sysoev on 07.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
